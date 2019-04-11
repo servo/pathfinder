affc7655521e52ae2666287bcf4ccb8fbb8c2701
Alan Jeffrey <ajeffrey@mozilla.com>
2019-04-10 21:10:17 -0500

    Added copyright statement to magicleap demo files

--- demo/magicleap/src/main.cpp
+++ demo/magicleap/src/main.cpp
@@ -1,3 +1,15 @@
+// pathfinder/demo/magicleap/src/main.cpp
+//
+// Copyright © 2019 The Pathfinder Project Developers.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// The immersive mode pathfinder magicleap demo
+
 #include <stdio.h>
 #include <stdlib.h>
 
