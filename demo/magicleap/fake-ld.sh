8b3bf7d0642cc9f95a243d0abb932c7cdca93004
Josh Matthews <josh@joshmatthews.net>
2019-04-15 13:56:17 -0400

    Use linker directly to to avoid linker errors.

--- demo/magicleap/fake-ld.sh
+++ demo/magicleap/fake-ld.sh
@@ -8,8 +8,8 @@ set -o nounset
 set -o pipefail
 
 TARGET=${TARGET:-"aarch64-linux-android"}
-LD=${LD:-"${MAGICLEAP_SDK}/tools/toolchains/bin/${TARGET}-gcc"}
-LDFLAGS=${LDFLAGS:-"--sysroot=${MAGICLEAP_SDK}/lumin -L${MAGICLEAP_SDK}/tools/toolchains/lib/gcc/${TARGET}/4.9.x ${MAGICLEAP_SDK}/lumin/usr/lib/crtbegin_so.o"}
+LD=${LD:-"${MAGICLEAP_SDK}/tools/toolchains/bin/${TARGET}-ld"}
+LDFLAGS=${LDFLAGS:-"--sysroot=${MAGICLEAP_SDK}/lumin -L${MAGICLEAP_SDK}/lumin/usr/lib -L${MAGICLEAP_SDK}/tools/toolchains/lib/gcc/${TARGET}/4.9.x ${MAGICLEAP_SDK}/lumin/usr/lib/crtbegin_so.o"}
 
 # Remove the -landroid flag, grr
 ARGS=("$@")
