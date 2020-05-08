6299d2c36b96463962ad56903c80fefad184a3e7
Patrick Walton <pcwalton@mimiga.net>
2020-05-07 21:26:41 -0700

    Clear the canvas to the background color if no drawing command did so.
    Closes #318.

---
+++ resources/shaders/gl3/clear.vs.glsl
@@ -0,0 +1,27 @@
+#version {{version}}
+// Automatically generated from files in pathfinder/shaders/. Do not edit!
+
+
+
+
+
+
+
+
+
+
+
+
+precision highp float;
+precision highp sampler2D;
+
+uniform vec4 uRect;
+uniform vec2 uFramebufferSize;
+
+in ivec2 aPosition;
+
+void main(){
+    vec2 position = mix(uRect . xy, uRect . zw, vec2(aPosition))/ uFramebufferSize * 2.0 - 1.0;
+    gl_Position = vec4(position . x, - position . y, 0.0, 1.0);
+}
+
